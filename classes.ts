1ad8be74afc99bffab80d6d48b9e50f2a48184c2
Bikal <bikaladhikari5@gmail.com>
2024-05-22 14:19:52 +1000

    Field in classes

--- classes.ts
+++ classes.ts
@@ -2,27 +2,35 @@ class Vehicle {
   // public drive(): void {
   //   console.log("chugga chugga");
   // }
+  // color: string;
+
+  constructor(public color: string) {
+    // this.color = color;
+  }
 
   protected honk(): void {
     console.log("beep");
   }
 }
 
-class Car extends Vehicle {
-  private drive(): void {
-    console.log("vroom");
-  }
+const vehicle = new Vehicle("orange");
+console.log(vehicle.color);
 
-  startDrivingProcess(): void {
-    this.drive();
-    this.honk();
-  }
-}
+// class Car extends Vehicle {
+//   private drive(): void {
+//     console.log("vroom");
+//   }
 
-// const vehicle = new Vehicle();
-// vehicle.drive();
-// vehicle.honk();
+//   startDrivingProcess(): void {
+//     this.drive();
+//     this.honk();
+//   }
+// }
 
-const car = new Car();
-car.startDrivingProcess();
+// // const vehicle = new Vehicle();
+// // vehicle.drive();
+// // vehicle.honk();
+
+// const car = new Car();
+// car.startDrivingProcess();
 // car.honk();
